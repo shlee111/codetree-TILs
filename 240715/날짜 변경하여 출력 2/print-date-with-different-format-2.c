6b5425ad447531fd877a3398ebec24b5fcf3bbb8
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-15 23:06:41 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/날짜 변경하여 출력 2/print-date-with-different-format-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int mm, dd, yyyy;
+
+    scanf("%d-%d-%d", &mm, &dd, &yyyy);
+    printf("%d.%d.%d", yyyy, mm, dd);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
