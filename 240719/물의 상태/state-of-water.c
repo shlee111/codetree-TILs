783e84e8f7a46e3a66cb85d314a1f5d8ef326022
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-19 19:06:58 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/물의 상태/state-of-water.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n<0){
+        printf("ice");
+    }
+    else if(n>=100){
+        printf("vapor");
+    }
+    else{
+        printf("water");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
