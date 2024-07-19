30fdd2b464f8e7668f9f702162a791ca833aef1c
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-19 16:48:47 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/입력받아 연산하기/take-input-and-operate.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d \n%d", &a, &b);
+
+    a += 87;
+    b %= 10;
+
+    printf("%d \n%d", a, b);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
