b73ebf7355cfc076c215182956144d5cf14bfcb1
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-19 19:18:56 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n>=3000){
+        printf("book");
+    }
+    else if(n>=1000){
+        printf("mask");
+    }
+    else{
+        printf("no");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
