8bdf50580bb475fe9a9926cc90d59b477b21c83b
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-24 19:08:41 +0900

    Title: 1부터 n까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/1부터 n까지 출력/print-from-1-to-n.c
+++ 240724/1부터 n까지 출력/print-from-1-to-n.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    for(int n;1 <= n;++n){
-        scanf("%d", &n);
-        printf("%d ", n);
+    int n, i;
+    scanf("%d", &n);
+    for(int i = 1 ; i <= n;++i){
+        printf("%d ", i);
     }
     // 여기에 코드를 작성해주세요.
     return 0;
