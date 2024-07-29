490cf66f630ff3d8a4ac2b4d99ba327448f2e4fa
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-29 16:50:04 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/n부터 1까지 숫자 출력/print-n-to-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int i=n;
+    while(i>=1){
+        printf("%d ", i);
+        i--;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
