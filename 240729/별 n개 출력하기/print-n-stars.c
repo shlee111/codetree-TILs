95d9d3c46a809abd34d05fae844627b0c250c09d
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-29 16:14:28 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/별 n개 출력하기/print-n-stars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 1;
+    scanf("%d", &n);
+
+    while(i<=n){
+        printf("*\n");
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
