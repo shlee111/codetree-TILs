55f3f34fb0357c4b48d090e009bc2cad3f3f3f56
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-07-31 19:09:39 +0900

    Title: 1부터의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/1부터의 합/sum-from-the-one.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum=0;
+    scanf("%d", &n);
+    for(int i=1; i<=100; i++){
+        sum += i;
+        if(sum >= n){
+            printf("%d", i);
+            break;
+        }            
+    }
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
