833217813e1764353979f026b356e79580112eef
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-08-06 17:47:40 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/모두 3의 배수/all-multiples-of-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e;
+    scanf("%d\n%d\n%d\n%d\n%d", &a, &b, &c, &d, &e);
+
+    if(a%3==0 && b%3==0 && c%3==0 && d%3==0 && e%3==0)
+        printf("1");
+    else
+        printf("0");
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
