01dd0a5f5593feea524d9bdd552bad8cbe85ac75
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-08-06 16:56:23 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/ab 사이에 없는 c/c-between-a-and-b-2.c
+++ 240806/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -7,10 +7,10 @@ int main() {
     bool satisfied = true;
 
     for(int i=a;i<=b;i++){
-        if(i%c!=0)
+        if(i%c == 0)
             satisfied = false;
     }
-    if(satisfied == false)
+    if(satisfied == true)
         printf("YES");
     else
         printf("NO");    
