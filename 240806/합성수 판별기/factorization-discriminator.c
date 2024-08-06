6d01dc771ae94d4446a4364ed693ccbaa8570ed0
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-08-06 16:12:31 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    bool satisfied = false;
+    for(int i=2;i<=(n-1);i++){
+        if((n-i)%i==0)
+            satisfied = true;
+    }
+    if(satisfied == true)
+        printf("C");
+    else
+        printf("N");
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
