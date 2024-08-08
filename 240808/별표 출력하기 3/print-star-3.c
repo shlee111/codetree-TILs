f3f8a36feb26c08615e806bebb33c0a3f822a632
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-08-08 18:15:45 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/별표 출력하기 3/print-star-3.c
+++ 240808/별표 출력하기 3/print-star-3.c
@@ -4,11 +4,11 @@ int main() {
     int n;
     scanf("%d",&n);
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<2i;j++){
-            printf(" ");
+    for(int i=n;i>0;i--){
+        for(int j=0;j<n-i;j++){
+            printf("  ");
         }
-        for(int j=2i+1;j>=0;j--){
+        for(int j=0;j<2*i-1;j++){
             printf("* ");
         }
         printf("\n");
