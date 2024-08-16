a59d947f176ece78e242275d9aa90979fbb55191
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-08-16 17:11:59 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/상하로 반복하며 출력/print-in-up-down-reverse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j%2==0){
+                printf("%d", i+1);
+            }
+            else{
+                printf("%d", n-i);
+            }
+        }
+    printf("\n");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
