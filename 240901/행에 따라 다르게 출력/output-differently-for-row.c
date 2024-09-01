2e1c0367de92bc30c1f2c2158359f2ef2c551c4c
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-09-01 20:55:22 +0900

    Title: 행에 따라 다르게 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240901/행에 따라 다르게 출력/output-differently-for-row.c
+++ 240901/행에 따라 다르게 출력/output-differently-for-row.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int a;
-    scanf("%d", &a);
-
-    int k = 0;
-    int num = 1;
-
-    for (int i = 0; i < a; i++) {
-        for (int j = 0; j < a; j++) {
-            if (i % 2 == 0) {
-                num = 1;
-            } else {
-                num = 2;
-            }
-            k = k + num;
-            printf("%d ", k);
-        }
-        printf("\n");
-    }
-
-    return 0;
+    // 변수 선언 및 입력
+    int n;
+    scanf("%d", &n);
+    int cnt = 0;
+	
+	// 값을 조건대로 증가시켜 출력합니다.
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			if(i % 2 == 0)
+                cnt++;
+            else
+                cnt += 2;
+            
+            printf("%d ", cnt);
+		}
+		printf("\n");
+	}
+	
+	return 0;
 }
