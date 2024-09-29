68aafd39aa64258a161b595604c06af30212f1ba
shlee111 <162542909+shlee111@users.noreply.github.com>
2024-09-29 20:18:54 +0900

    Title: 나눗셈의 나머지, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240929/나눗셈의 나머지/remainder-of-division.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, i, arr[10], sum=0;
+    int cnt_arr[11]={0,};
+
+    scanf("%d %d", &a, &b);
+    
+    for(i=a;i>=1;i/=b){
+        arr[i] = i%b;
+        cnt_arr[arr[i]]++;
+    }
+    for(i=0;i<10;i++){
+        sum += cnt_arr[i]*cnt_arr[i];
+    }
+    printf("%d", sum);
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
